4003f3543482ed280f10c53713cbb6ea968271fe
Franco Mazzoni <franco.mazzoni73@gmail.com>
2021-06-26 16:24:51 -0300

    Fix compiling

--- contract/contract.sol
+++ contract/contract.sol
@@ -1,11 +1,14 @@
 pragma solidity >=0.5.0 <0.6.0;
 
+import "./ownable.sol";
+
 contract class is Ownable {
     
     uint minRate = 4;
     uint maxRate = 10;
     
     struct Course {
+        uint id;
         string name;
         address prof;
         uint credits;
@@ -26,40 +29,31 @@ contract class is Ownable {
     mapping (address => mapping (uint => Approval)) public studentAddressAndCourseIdToApproval;
     mapping (uint => Approval[]) public courseIdToApprovals;
 
-    function createCourse(string _name, string _prof, uint _credits, uint[] _correlatives) public {
-        require(_name != '' && _prof != '' && _credits != ''); //TODO: Ver el tema de validacion de correlativas
-        courses.push(Course(_name, profNameToAddress[_prof], _credits, _correlatives, true));
+    function createCourse(uint _id, string memory _name, string memory _prof, uint _credits, uint[] memory _correlatives) public {
+        courses.push(Course(_id, _name, profNameToAddress[_prof], _credits, _correlatives, true));
         courseToOwner[_id] = msg.sender;
     }
     
-    function editCourse(uint _id, string _name, string _prof, uint _credits, uint[] _correlatives, bool active) public {
-        Course courseToChange = courses[_id];
-        if (_name != '') {
-            courseToChange.name = _name;
-        }
-        if (_prof != '') {
-            courseToChange.prof = profNameToAddress[_prof];
-        }
-        if (_credits != '') {
-            courseToChange.credits = _credits;
-        }
-        if (_correlatives != '') {
-            courseToChange.correlatives = _correlatives;
-        }
-        if (_active != '') {
-            courseToChange.active = _active;
-        }
+    function editCourse(uint _id, string memory _name, string memory _prof, uint _credits, uint[] memory _correlatives, bool _active) public {
+        require(msg.sender == courses[_id].prof);
+        Course storage courseToChange = courses[_id];
+        courseToChange.name = _name;
+        courseToChange.prof = profNameToAddress[_prof];
+        courseToChange.credits = _credits;
+        courseToChange.correlatives = _correlatives;
+        courseToChange.active = _active;
     }
     
     function approveStudent(address _student, uint _courseId, bool _partialApproval, uint _rate) public {
+        require(msg.sender == courses[_courseId].prof);
         if (_partialApproval) {
             //TODO: verificar como hacer para que newApproval sea modificado en todos los mappings en los que esta
-            Approval newApproval = Approval(_partialApproval, 0, now);
+            Approval memory newApproval = Approval(_partialApproval, 0, now);
             studentAddressAndCourseIdToApproval[_student][_courseId] = newApproval;
             courseIdToApprovals[_courseId].push(newApproval);
         } else {
             require(_rate >= minRate && _rate <= maxRate);
-            Approval studentApprovalForCourse = studentAddressAndCourseIdToApproval[_student][_courseId];
+            Approval memory studentApprovalForCourse = studentAddressAndCourseIdToApproval[_student][_courseId];
             studentApprovalForCourse.partialApproval = false;
             studentApprovalForCourse.rate = _rate;
             //TODO: agregar token para creditos
@@ -71,20 +65,14 @@ contract class is Ownable {
         
     }
     
-    //TODO: preguntar el miercoles. Capaz se puede hacer en la funcion anterior
-    function verifyPartialApprovalForStudent(uint _courseId) {
-        Approval[] approvalsForCourse = courseIdToApprovals[_courseId];
-        Approval[] partialApprovals = new Approval[];
+    function verifyPartialApprovalForStudent(uint _courseId) public {
+        Approval[] memory approvalsForCourse = courseIdToApprovals[_courseId];
         for (uint i = 0; i < approvalsForCourse.length; i++) {
-            if (approvalsForCourse[i].partialApproval) {
-                partialApprovals.push(approvalsForCourse[i]);
+            if (approvalsForCourse[i].partialApproval && now >= (approvalsForCourse[i].approvalDate + 548 days)) {
+                 //TODO: remover curso para alumno
             }
         }
-        for (uint i = 0; i < partialApprovals.length; i++) {
-            if (now >= (partialApprovals[i].approvalDate + 548 days)) {
-                //TODO: remover curso para alumno
-            }
-        }
+        
     }
     
 }
