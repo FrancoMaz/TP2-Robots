300247c3a9ad54f8c9be8065e38fcdd2003b9260
Franco Mazzoni <franco.mazzoni73@gmail.com>
2021-06-26 17:18:28 -0300

    Add correlatives checking and get approvals by student

--- contract/contract.sol
+++ contract/contract.sol
@@ -1,4 +1,5 @@
 pragma solidity >=0.5.0 <0.6.0;
+pragma experimental ABIEncoderV2;
 
 import "./ownable.sol";
 
@@ -17,6 +18,8 @@ contract class is Ownable {
     }
     
     struct Approval {
+        uint courseId;
+        address student;
         bool partialApproval;
         uint rate;
         uint approvalDate;
@@ -26,19 +29,18 @@ contract class is Ownable {
     
     mapping (uint => address) public courseToOwner;
     mapping (string => address) public profNameToAddress;
-    mapping (address => mapping (uint => Approval)) public studentAddressAndCourseIdToApproval;
+    mapping (address => Approval[]) public studentAddressToApprovals;
     mapping (uint => Approval[]) public courseIdToApprovals;
 
-    function createCourse(uint _id, string memory _name, string memory _prof, uint _credits, uint[] memory _correlatives) public {
-        courses.push(Course(_id, _name, profNameToAddress[_prof], _credits, _correlatives, true));
+    function createCourse(uint _id, string memory _name, address _prof, uint _credits, uint[] memory _correlatives) public {
+        courses.push(Course(_id, _name, _prof, _credits, _correlatives, true));
         courseToOwner[_id] = msg.sender;
     }
     
-    function editCourse(uint _id, string memory _name, string memory _prof, uint _credits, uint[] memory _correlatives, bool _active) public {
-        require(msg.sender == courses[_id].prof);
+    function editCourse(uint _id, string memory _name, address _prof, uint _credits, uint[] memory _correlatives, bool _active) public {
         Course storage courseToChange = courses[_id];
         courseToChange.name = _name;
-        courseToChange.prof = profNameToAddress[_prof];
+        courseToChange.prof = _prof;
         courseToChange.credits = _credits;
         courseToChange.correlatives = _correlatives;
         courseToChange.active = _active;
@@ -46,23 +48,47 @@ contract class is Ownable {
     
     function approveStudent(address _student, uint _courseId, bool _partialApproval, uint _rate) public {
         require(msg.sender == courses[_courseId].prof);
+        require(correlativesApproved(_student, _courseId));
         if (_partialApproval) {
-            //TODO: verificar como hacer para que newApproval sea modificado en todos los mappings en los que esta
-            Approval memory newApproval = Approval(_partialApproval, 0, now);
-            studentAddressAndCourseIdToApproval[_student][_courseId] = newApproval;
-            courseIdToApprovals[_courseId].push(newApproval);
+            studentAddressToApprovals[_student].push(Approval(_courseId, _student, _partialApproval, 0, now));
+            courseIdToApprovals[_courseId].push(Approval(_courseId, _student, _partialApproval, 0, now));
         } else {
             require(_rate >= minRate && _rate <= maxRate);
-            Approval memory studentApprovalForCourse = studentAddressAndCourseIdToApproval[_student][_courseId];
-            studentApprovalForCourse.partialApproval = false;
-            studentApprovalForCourse.rate = _rate;
+            for (uint i = 0; i < studentAddressToApprovals[_student].length; i++) {
+                if (studentAddressToApprovals[_student][i].courseId == _courseId) {
+                    studentAddressToApprovals[_student][i].partialApproval = false;
+                    studentAddressToApprovals[_student][i].rate = _rate;
+                }
+            }
+            for (uint i = 0; i < courseIdToApprovals[_courseId].length; i++) {
+                if (courseIdToApprovals[_courseId][i].student == _student) {
+                    courseIdToApprovals[_courseId][i].partialApproval = false;
+                    courseIdToApprovals[_courseId][i].rate = _rate;
+                }
+            }
             //TODO: agregar token para creditos
         }
         //TODO: enviar evento de aprobacion de la materia
     }
     
-    function verifyStudentState(address _student) public {
-        
+    function correlativesApproved(address _student, uint _courseId) private view returns (bool) {
+        uint approved = 0;
+        for (uint i = 0; i < courses[_courseId].correlatives.length; i++) {
+            for (uint j = 0; j < studentAddressToApprovals[_student].length; j++) {
+                if (studentAddressToApprovals[_student][j].courseId == courses[_courseId].correlatives[i]) {
+                    approved++;
+                }
+            }
+        }
+        if (approved == courses[_courseId].correlatives.length) {
+            return true;
+        }
+        return false;
+    }
+    
+    //Funcion para verificar el estado del alumno en todas las materias
+    function getApprovalsByStudent(address _student) public returns (Approval[] memory) {
+        return studentAddressToApprovals[_student];
     }
     
     function verifyPartialApprovalForStudent(uint _courseId) public {
